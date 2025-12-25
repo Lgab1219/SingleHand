3ac8889f3118bcfbd558209b93f9041dac583324
lgab1219 <lancegabrielmaghanoy@gmail.com>
2025-12-25 14:06:39 +0800

    feature: authentication change detector

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,26 @@
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
 import { ScrollView, StyleSheet } from "react-native";
+import { supabase } from "../scripts/supabase";
 import Registration from "./registration";
 
 export default function RootScreen() {
+
+    const router = useRouter();
+
+    // Check if there is a change in auth state. If there is, navigate to selected path.
+    useEffect(() => {
+        const { data: listener } = supabase.auth.onAuthStateChange(
+            (_event, session) => {
+                if (session) {
+                    router.replace("/(tabs)/explore");
+                }
+            }
+        );
+
+        return () => listener.subscription.unsubscribe();
+    }, []);
+
     return (
         <ScrollView style={styles.screen}>
             <Registration />
